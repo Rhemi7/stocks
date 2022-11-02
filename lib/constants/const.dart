4c0b630a16cf49dda09a327c4892e0572c851fb7
aderemilekun <adelefatai@yahoo.com>
2022-11-02 14:44:42 +0100

    changed api key

--- lib/constants/const.dart
+++ lib/constants/const.dart
@@ -1,5 +1,5 @@
 
 const String baseUrl = "http://api.marketstack.com/v1";
-const String apikey = "f9c7e1dd5ac6eddb51ef56a4462d9e53";
+const String apikey = "48301d9edf11b295c7049221805e2437";
 const List<String> symbols = ["AAPL","GOOGL","ADBE","NIO","NVS","NFLX","ABT","NKE","C","SAP"];
 const Map<String, String> headers = {"Content-Type": "application/json"};
